85924c810885a1b87765904d3b058838e6cccd9c
Srujan <srujankumar123@gmail.com>
2016-11-04 16:57:59 +0530

    Added Variable_file.pp

--- Variable_file.pp
+++ Variable_file.pp
@@ -1,12 +1,13 @@
 $owner = 'root'
 $group = 'root'
 $mode  = '0644'
+
 file { '/temp.txt':
     ensure => file,
     owner  => $owner,
     group  => $group,
     mode   => $mode,
-    content => "This is from Puppet",
+    content => "This is from Puppet \n The owner is ${owner} \n The group is ${group}",
     
 }
 
