c6cf3051d03f7342d29564d285d37214c3865875
Srujan <srujankumar123@gmail.com>
2016-11-04 17:19:05 +0530

    Modified Variable_file.pp

--- Variable_file.pp
+++ Variable_file.pp
@@ -5,10 +5,10 @@ $filecontent = "This is from Puppet \n The owner is ${owner} \n The group is ${g
 
 file { '/temp.txt':
     ensure => file,
-    owner  => owner,
+    owner  => $owner,
     group  => $group,
     mode   => $mode,
-    content => $filecontent,
+    content => filecontent,
     
 }
 
