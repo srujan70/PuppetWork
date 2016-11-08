381853674b2e730522e2bd7447a97dbceabd49a6
Srujan <srujankumar123@gmail.com>
2016-11-08 23:39:27 +0530

    Added ntp_config.pp

---
+++ ntp_config.pp
@@ -0,0 +1,45 @@
+$ntp_conf=@(END)
+#Managed by Puppet 
+Server in.pool.ntp.org iburst prefer 
+driftfile /var/lib/ntp/drift 
+END
+
+case $facts['os']['family'] {
+    'Redhat': {
+        $ntp_service='ntpd'
+        $admingroup='wheel'
+    }
+    'Debian': {
+        $ntp_service='ntp'
+        $admingroup='sudo'
+    }
+    default :{
+        fail("your $facts['os']['family'] is not supported ")
+    }
+     
+}
+
+package { 'ntp':
+    ensure => installed,
+    before => File['/etc/ntp_conf'],
+}
+
+File {
+    owner => 'root',
+    group => $admingroup,
+    mode => '0664',
+    ensure => 'file',
+}
+
+file { '/etc/ntp.conf':
+    content => $ntp_conf,
+    notify => Service['Ntp_Service'],
+    
+}
+
+service { "Ntp_Service":
+    ensure     => running,
+    enable     => true,
+    name       =>$ntp_service,
+    subscribe  => File['/etc/ntp_conf'],
+}
